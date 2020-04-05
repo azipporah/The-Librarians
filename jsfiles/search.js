fdc3764081200de8abbd8d1c90fd66e64b2e72eb
azipporah <zipporahatusasiire@gmail.com>
2020-04-05 23:59:45 +0200

    Adding more changes

--- jsfiles/search.js
+++ jsfiles/search.js
@@ -19,3 +19,22 @@ function myFunction() {
     }
   }
 }
+
+
+   // Loop through the ul list and hide those book titles that don't match the search query
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
